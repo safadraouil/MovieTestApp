e0166950e8fe63f4af5f18cc129da8a046432efe
draouilsafa <draouilsafa@gmail.com>
2020-07-03 19:34:49 +0100

    fix design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";  
-import Items from './items'
-import _ from "lodash/fp";
+import React, { useState  } from "react";  
+import Items from './items' 
 import { useForm } from "react-hook-form";
-import styled from 'styled-components'; 
-
+import styled from 'styled-components';
+ import 'bootstrap/dist/css/bootstrap.css';  
+ 
 function App() {
   const { register, handleSubmit  } = useForm();
   const [items, setItems] = useState(["first", "second"]); 
@@ -11,100 +11,123 @@ function App() {
    setItems(items => items.concat(data));  
  
   };  
-  const Button = styled.button`
-
-  display: inline-block;
-  color: palevioletred;
-  font-size: 1em;
-  margin-left: 16em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-  display: block;
-  width: 160px;
-  height: 30px
-  
+  const Buttons = styled.button`
+  margin :10em;
+  margin-left:50%;
+  width:10%;
+  border : 1px solid #2FA4E7;
+  background: none;
+ 
+`;
+const Inputs = styled.input`
+margin-left:20em;
+  padding: 0.5em; 
+  border: none  ;
+  border-radius: none;
+  border-bottom: 1px solid #2FA4E7;
+  margin-bottom: 1em;
+  width:70%;
 `;
 
+const Labels = styled.label`
 
-const Inputs = styled.input`
-margin-left: 6em;
-  padding: 0.5em;
-  color:   "palevioletred";
-  color: palevioletred;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
+  margin-left:20em;
+  padding: 0.5em; 
+  border: none  ; 
   margin-bottom: 1em;
+  color:#2FA4E7;
 `;
 
 const Textareas = styled.textarea`
-margin-left: 3em;
-  padding: 0.5em;
-  color:   "palevioletred"; 
-  margin-bottom: 1em;
-  color: palevioletred;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
+  width:70%;
+  margin-left: 20em;
+  padding: 0.5em; 
+  margin-bottom: 2em; 
+  border: none  ;
+  border-radius: none;
+  border-bottom: 1px solid #2FA4E7;
 `;
 
- 
+const Lis = styled.li`
+    list-style-type: none;
+`;
 const Titl = styled.h1({ 
+  marginTop: "2em",
   textAlign: "center"
 
 });
-
-
-
-const card = styled.div({ 
-
+const Card = styled.div({ 
   color:  "palevioletred",
   borderRadius: '3px'
- 
-
-});
+ });  
+ const Formscard = styled.form`
+ marginTop: "10em",
+ display: block; 
+ `;
  
   return (
     <>
-    <card>
-      <Titl>Movies List</Titl> 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <li>
-      <label>Name</label>
-      <Inputs
-       class="form-control"
-        name="Name"
-        ref={register({
-          required: true,
-          maxLength: 20,
-          pattern: /^[A-Za-z]+$/i
-        })}
-      /> 
+    <div>  
+    <div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a className="navbar-brand" href="_blank">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarColor01">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="_blank">Home <span className="sr-only">(current)</span></a>
       </li>
-      <li>
-      <label>Description</label> 
-      
-      <Textareas
-        name="Description" 
-        ref={register({
-          required: true, 
-        })}
-      /> </li> 
-      <li>
-      <label>Url</label>
-      <Inputs
-       class="form-control-lg"
-        name="Url" 
-        value={"https://media.w3.org/2010/05/sintel/trailer_hd.mp4"} 
-        ref={register({
-          required: true, 
-        })}
-      /> 
+      <li className="nav-item">
+        <a className="nav-link" href="_blank">Features</a>
       </li>
-   
-       
-      <Button type="submit"   >'New project ✅'</Button>
- 
+      <li className="nav-item">
+        <a className="nav-link" href="_blank">Pricing</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="_blank">About</a>
+      </li>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
+      <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
     </form>
+  </div>
+</nav>
+</div>
+    <Card>
+      <Titl>Videos List</Titl> 
+      <Formscard onSubmit={handleSubmit(onSubmit)}>
+        <Lis>
+      <div className="form-group">
+      <Labels className="col-form-label" htmlFor="inputDefault">Name</Labels>
+      <Inputs   name="Name" ref={register({
+      required: true,
+      maxLength: 20,
+      pattern: /^[A-Za-z]+$/i
+      })} type="text" className="form-control" placeholder="input Name" id="inputDefault"/>
+      </div>  
+      </Lis>
+      <Lis>
+  
+      <div className="form-group">
+      <Labels htmlFor="exampleTextarea">Description</Labels>
+      <Textareas   name="Description"  ref={register({
+      required: true, 
+      })}  className="form-control" id="exampleTextarea" rows="3"></Textareas>
+      </div> 
+      </Lis> 
+      <Lis> 
+      <div className="form-group">
+      <Labels className="col-form-label" htmlFor="inputDefault">Url</Labels>
+      <Inputs   name="Url"   ref={register({ required: true,  })} type="text" className="form-control" placeholder="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" id="inputDefault"     />
+      </div>  
+      </Lis>
+   <div>
+      <Buttons type="submit" className="btn btn-primary" > ✅</Buttons>
+      </div>
+    </Formscard>
       {
         <Items
           items={items} 
@@ -113,7 +136,8 @@ const card = styled.div({
           }
         />
       }
-      </card>
+      </Card>
+      </div>
     </>
   );
 }
--- src/items.js
+++ src/items.js
@@ -1,54 +1,66 @@
-import React, { useState, useEffect } from "react"; 
+import React from "react"; 
 import styled from 'styled-components';
 import { Player } from 'video-react'; 
-
-
 function Items(props) {
     const { items, removeHandler } = props;  
     const Box = styled.div({ 
-      textAlign: "center"
-  
+    backgroundColor:'#F8F8FF',
+    padding: '3px 50px 3px 50px',
     });
     const VideoBox = styled.div({  
-      marginLeft: '40%',
-      height: '40%',
-      width: '25%'
+    marginLeft: '40%',
+    marginTop: '3%',
+    height: '40%',
+    width: '25%',
     }); 
+    const Lis = styled.li`
+    list-style-type: none;
+    `;
+    const Buttons = styled.button`
+    margin :1em;
+    margin-left:90%;
+    width:10%;
+    border : 1px solid #2FA4E7;
+    background: none;
 
-
- 
+    `;
     return (
-  
       <Box>
       <div> 
-       
         <ol>
           {items.map((data, index) => (
-            <li
+            <Lis
               key={index} 
             > 
             <VideoBox> 
                <Player>
-                <source src={data.Url != undefined ? data.Url : "https://media.w3.org/2010/05/sintel/trailer_hd.mp4" }/>
+                <source src={data.Url !== undefined ? data.Url : "https://media.w3.org/2010/05/sintel/trailer_hd.mp4" }/>
                 </Player>
-                
-            </VideoBox> 
-
-            <div>NAME:  {data.Name}</div>
-            <div>Description: {data.Description}</div> 
-           
-            
-              <button onClick={() => removeHandler(index)}>
+              <div className="list-group">
+              <a href="_blank" className="list-group-item list-group-item-action flex-column align-items-start active">
+              <div className="d-flex w-100 justify-content-between">
+              <h3 className="mb-1">NAME: </h3>
+              <h5> {data.Name}</h5>
+              </div>          
+              </a>
+              <a href="_blank" className="list-group-item list-group-item-action flex-column align-items-start">
+              <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">    Description:  </h5>   
+              </div>
+              <p className="mb-1">{data.Description}</p> 
+              <Buttons onClick={() => removeHandler(index)} className = "btn btn-secondary">
                 <span role="img" aria-label="remove">
                   ❌
-                </span>
-          
-              </button>
-            </li>
-          ))}
+                </span>  
+              </Buttons>
+              </a>
+              </div>   
+              
+            </VideoBox>     
+            </Lis>
+          ))}  
         </ol>
-      
-        </div>
+        </div> 
         </Box>
     );
   }
